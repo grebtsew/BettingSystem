9a20aa927850b1f310c3b0a7115a081496406b0a
GREBTSEW <danwe980@student.liu.se>
2018-01-17 23:20:27 +0100

    Fixed the functionallity, now it works kinda good.
    Easy fast betting system v1.0

--- BettingSystem/BettingSystem/Form1.Designer.cs
+++ BettingSystem/BettingSystem/Form1.Designer.cs
@@ -28,10 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.label1 = new System.Windows.Forms.Label();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
@@ -39,42 +40,55 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // chart1
             // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.chart1.BackColor = System.Drawing.Color.Black;
             this.chart1.BackImageTransparentColor = System.Drawing.Color.Lime;
             this.chart1.BackSecondaryColor = System.Drawing.Color.Lime;
             this.chart1.BorderlineColor = System.Drawing.Color.Black;
-            chartArea2.AlignmentOrientation = ((System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations)((System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations.Vertical | System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations.Horizontal)));
-            chartArea2.BackColor = System.Drawing.Color.Silver;
-            chartArea2.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea2);
-            legend2.Enabled = false;
-            legend2.Name = "Legend1";
-            this.chart1.Legends.Add(legend2);
+            chartArea1.AlignmentOrientation = ((System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations)((System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations.Vertical | System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations.Horizontal)));
+            chartArea1.BackColor = System.Drawing.Color.Silver;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Enabled = false;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
             this.chart1.Location = new System.Drawing.Point(12, 12);
             this.chart1.Name = "chart1";
-            series3.BorderWidth = 5;
+            series1.BorderWidth = 5;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Series2";
+            series1.YValuesPerPoint = 4;
+            series2.BorderWidth = 5;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Color = System.Drawing.Color.Lime;
+            series2.Legend = "Legend1";
+            series2.MarkerBorderColor = System.Drawing.Color.Black;
+            series2.Name = "Series1";
+            series2.YValuesPerPoint = 4;
+            series3.BorderWidth = 10;
             series3.ChartArea = "ChartArea1";
-            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
             series3.Legend = "Legend1";
-            series3.Name = "Series2";
-            series3.YValuesPerPoint = 4;
-            series4.BorderWidth = 5;
-            series4.ChartArea = "ChartArea1";
-            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series4.Color = System.Drawing.Color.Lime;
-            series4.Legend = "Legend1";
-            series4.MarkerBorderColor = System.Drawing.Color.Black;
-            series4.Name = "Series1";
-            series4.YValuesPerPoint = 4;
+            series3.Name = "Series3";
+            series3.YValuesPerPoint = 2;
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
             this.chart1.Series.Add(series3);
-            this.chart1.Series.Add(series4);
-            this.chart1.Size = new System.Drawing.Size(1724, 748);
+            this.chart1.Size = new System.Drawing.Size(2577, 1042);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
             this.chart1.Click += new System.EventHandler(this.chart1_Click);
@@ -85,7 +99,7 @@
             this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Lime;
-            this.label1.Location = new System.Drawing.Point(23, 41);
+            this.label1.Location = new System.Drawing.Point(151, 51);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(126, 46);
             this.label1.TabIndex = 2;
@@ -94,10 +108,11 @@
             // 
             // numericUpDown1
             // 
+            this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.numericUpDown1.Location = new System.Drawing.Point(349, 782);
+            this.numericUpDown1.Location = new System.Drawing.Point(349, 1076);
             this.numericUpDown1.Maximum = new decimal(new int[] {
-            10,
+            28,
             0,
             0,
             0});
@@ -108,9 +123,10 @@
             // 
             // label2
             // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(23, 782);
+            this.label2.Location = new System.Drawing.Point(23, 1076);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(320, 46);
             this.label2.TabIndex = 4;
@@ -118,10 +134,11 @@
             // 
             // button1
             // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(548, 782);
+            this.button1.Location = new System.Drawing.Point(548, 1066);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(167, 40);
+            this.button1.Size = new System.Drawing.Size(173, 66);
             this.button1.TabIndex = 5;
             this.button1.Text = "+";
             this.button1.UseVisualStyleBackColor = true;
@@ -129,10 +146,11 @@
             // 
             // button2
             // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(738, 782);
+            this.button2.Location = new System.Drawing.Point(727, 1066);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(167, 40);
+            this.button2.Size = new System.Drawing.Size(183, 66);
             this.button2.TabIndex = 6;
             this.button2.Text = "-";
             this.button2.UseVisualStyleBackColor = true;
@@ -140,18 +158,59 @@
             // 
             // label3
             // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(969, 782);
+            this.label3.Location = new System.Drawing.Point(969, 1076);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(0, 46);
             this.label3.TabIndex = 7;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Random Number Flow"});
+            this.comboBox1.Location = new System.Drawing.Point(2124, 1077);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(465, 48);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(1931, 1079);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(187, 46);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Data flow";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Lime;
+            this.label5.Location = new System.Drawing.Point(12, 51);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(133, 46);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Value:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1748, 850);
+            this.ClientSize = new System.Drawing.Size(2601, 1144);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -178,6 +237,9 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- BettingSystem/BettingSystem/Form1.cs
+++ BettingSystem/BettingSystem/Form1.cs
@@ -19,14 +19,32 @@ namespace BettingSystem
 
         public Thread chartUpdateThread;
         public double[] valueArray = new double[30];
-        public double[] tempArray = new double[40];
         public double START_VALUE = 1;
         public int selected_ticks = 0;
+        public double full_change = 0;
+        public bool bet_started = false;
+        public double bet_start_value = 0;
+        public double flow_value = 0;
+        public bool plus = false;
+        public double bet_approx = 0;
+        public double d1 = 0;
+        public double d2 = 0;
+        public double delta_change = 0;
+        public Random rnd = new Random();
+        public double change_value = 0;
+
+        public const int SLEEP_TIME = 200;
+        public const double VALUE_BALANCE = SLEEP_TIME / 2.5;
+        public const int DERIVATE_NUMBER_AMOUNT = 3;
+        public const double FIFTY_PERCENT = 0.5;
 
 
         public Form1()
         {
             InitializeComponent();
+
+
+            comboBox1.SelectedIndex = 0;
 
             chartUpdateThread = new Thread(new ThreadStart(this.run));
             chartUpdateThread.IsBackground = true;
@@ -34,55 +52,113 @@ namespace BettingSystem
             
         }
 
+        private void bet_is_over()
+        {
+            if ((bet_start_value - flow_value < 0 && plus) || (bet_start_value - flow_value > 0 && !plus))
+            {
+
+                if (this.label3.InvokeRequired)
+                {
+                    this.label3.BeginInvoke((MethodInvoker)delegate () { this.label3.Text = "Won! Diff: " + (-(bet_start_value - flow_value)).ToString(); });
+                }
+                else
+                {
+                    this.label3.Text = "Won! Diff: " + (-(bet_start_value - flow_value)).ToString();
+                }
+            }
+            else
+            {
+
+                if (this.label3.InvokeRequired)
+                {
+                    this.label3.BeginInvoke((MethodInvoker)delegate () { this.label3.Text = "Lost! Diff: " + (-(bet_start_value - flow_value)).ToString(); });
+                }
+                else
+                {
+                    this.label3.Text = "Lost! Diff: " + (-(bet_start_value - flow_value)).ToString();
+                }
+            }
+        }
+
+        private void approx_position()
+        {
+            if (selected_ticks > 0)
+            {
+                full_change = flow_value;
+                d1 = flow_value;
+                d2 = valueArray[valueArray.Length - DERIVATE_NUMBER_AMOUNT];
+                delta_change = d2 - d1;
+
+
+
+                for (int i = 0; i < selected_ticks; i++)
+                {
+                    full_change += delta_change;
+                }
+
+
+            }
+
+        }
+
+        private void generate_data_flow()
+        {
+            if (rnd.NextDouble() < FIFTY_PERCENT)
+            {
+                change_value = -rnd.NextDouble() / VALUE_BALANCE;
+            }
+            else
+            {
+                change_value = rnd.NextDouble() / VALUE_BALANCE;
+            }
+
+
+            flow_value += change_value;
+            valueArray[valueArray.Length - 1] = flow_value;
+            Array.Copy(valueArray, 1, valueArray, 0, valueArray.Length - 1);
+        }
+
+
         private void run()
         {
-            double value = START_VALUE;
-            double change_value = 0;
-            double d1 = 0;
-            double d2 = 0;
-            double delta_change = 0;
 
-
-
-            Random rnd = new Random();
+            flow_value = START_VALUE;
 
             while (true)
             {
-                if (rnd.NextDouble() < 0.5) { 
-                change_value = - rnd.NextDouble() / 100;
-                } else
+
+                if(selected_ticks <= 0)
                 {
-                 change_value = rnd.NextDouble() / 100;
-                }
-                
-
-
-                    value = value + change_value;
-
-                valueArray[valueArray.Length - 1] = value;
-                Array.Copy(valueArray, 1, valueArray, 0, valueArray.Length - 1);
-
-                // pressumption
-
-                if(selected_ticks > 0)
-                {
-                    Array.Clear(tempArray, 0, tempArray.Length);
-                    
-                    
-                 d1 = value;
-                 d2 = valueArray[valueArray.Length - 2];
-                delta_change = d2 - d1;
-                    Console.WriteLine(1000*delta_change);
-                for(int i = 0; i < selected_ticks; i++)
+                    if (bet_started)
                     {
-                        tempArray[tempArray.Length -2- 10+selected_ticks] = value + delta_change;
-                        Array.Copy(tempArray, 1, tempArray, 0, tempArray.Length - 1);
+                        bet_is_over();
+                        bet_started = false;
                     }
-
                 }
 
+                // count down for bet
+                if (bet_started)
+                {
+                    selected_ticks--;
 
-                // chart
+                    if (this.numericUpDown1.InvokeRequired)
+                    {
+                        this.numericUpDown1.BeginInvoke((MethodInvoker)delegate () { this.numericUpDown1.Value = selected_ticks; });
+                    }
+                    else
+                    {
+                        this.numericUpDown1.Value = selected_ticks;
+                    }
+                }
+
+                // Generate a flow of data, something to bet on
+                generate_data_flow();
+
+                // Approx curve
+                approx_position();
+ 
+
+                // update chart
                 if (chart1.IsHandleCreated)
                 {
                     this.Invoke((MethodInvoker)delegate { UpdateChart(); });
@@ -91,18 +167,18 @@ namespace BettingSystem
                     // ....
                 }
 
-                // labels
+                // update value label
                 if (this.label1.InvokeRequired)
                 {
-                    this.label1.BeginInvoke((MethodInvoker)delegate () { this.label1.Text = value.ToString();  });
+                    this.label1.BeginInvoke((MethodInvoker)delegate () { this.label1.Text = flow_value.ToString();  });
                 }
                 else
                 {
-                    this.label1.Text = value.ToString(); 
+                    this.label1.Text = flow_value.ToString(); 
                 }
 
-
-                Thread.Sleep(500);
+                // rest a while
+                Thread.Sleep(SLEEP_TIME);
             }
 
         }
@@ -112,24 +188,25 @@ namespace BettingSystem
         {
             chart1.Series["Series1"].Points.Clear();
             chart1.Series["Series2"].Points.Clear();
+            chart1.Series["Series3"].Points.Clear();
 
             foreach (double d in valueArray)
             {
 
                 chart1.Series["Series1"].Points.AddY(d);
 
-                
-
             }
 
             if (selected_ticks > 0)
             {
+                chart1.Series["Series2"].Points.AddXY(valueArray.Length, valueArray[valueArray.Length-1]);
+                chart1.Series["Series2"].Points.AddXY(valueArray.Length + selected_ticks, full_change);
+            }
 
-                foreach (double j in tempArray)
-                {
-                  
-                    chart1.Series["Series2"].Points.AddY(j);
-                }
+            if (bet_started)
+            {
+                chart1.Series["Series3"].Points.AddXY(valueArray.Length + selected_ticks, bet_approx);
+                
             }
 
         }
@@ -156,19 +233,40 @@ namespace BettingSystem
 
         private void button1_Click_1(object sender, EventArgs e)
         {
-            // pluss
-
+            // plus
+            if (!bet_started)
+            {
+                plus = true;
+                bet_started = true;
+                bet_start_value = flow_value;
+                bet_approx = full_change;
+                label3.Text = "Positive bet started please wait...";
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            // negative
 
+            // negative
+            if (!bet_started) {
+            plus = false;
+            bet_started = true;
+            bet_start_value = flow_value;
+            bet_approx = full_change;
+
+            label3.Text = "Negative bet started please wait...";
+            }
         }
 
         private void numericUpDown1_ValueChanged(object sender, EventArgs e)
         {
             selected_ticks =(int) numericUpDown1.Value;
+           
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
